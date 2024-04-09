bd521d14287755b92cabc120a00ee1ead4e090c1
jepguzman <69858958+jepguzman@users.noreply.github.com>
2024-04-09 00:00:32 -0600

    Add files via upload

---
+++ verImagenes.php
@@ -0,0 +1,16 @@
+<?php
+
+//incluimos ReadBeanPHP
+require './rb/rb.php';
+// Inicializamos la conexion al Servidor de MySQL
+R::setup( 'mysql:host=localhost;dbname=galeria','root', '' ); 
+
+$imgs = R::findAll( 'imagenes' );
+
+echo "<ul>";
+foreach( $imgs as $img ) {
+    echo "<li>{$img->id}: {$img->descripcion}, Likes {$img->likes} </li>";
+  }
+ echo "</ul>"; 
+
+?>
